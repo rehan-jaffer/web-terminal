b29da3b314f3fcde8d4dc099831d3e18f1bc6455
Ray <ray@thelondonvandal.com>
2015-10-18 15:07:56 +0000

    added ability to open a connection and send data back and forth

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,42 @@
+class Terminal {
+
+  constructor() {
+  
+    try {
+      this.connection = new WebSocket("ws://localhost:16000/");
+	
+      this.connection.onmessage = msg => {
+        $(".output-console").text(msg.data);
+      };
+
+      this.connection.onopen = function() {
+        this.send("test");
+      };
+    } catch(e) {
+      console.log("Failed to connect!" + e);
+    }
+
+  }
+
+  send(msg) {
+
+    try {
+      this.connection.send(msg);
+    } catch(e) {
+      console.log(e);
+    }
+
+  }
+
+}
+
 $(document).ready(function() {
+
+  var terminal = new Terminal;
 
   $('.secret-input').bind("enterKey", function (e) {
     console.log($(".secret-input").val());
+    terminal.send($(".secret-input").val());
     $(".secret-input").val("");
   });
   $('.secret-input').keyup(function (e) {
