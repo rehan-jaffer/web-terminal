6be7e149177f0bb17180672f7abdfb3acf91c656
Ray <ray@thelondonvandal.com>
2015-10-18 14:08:11 +0000

    added all initial files (src/dist directories, gulpfile, stylesheet)

---
+++ src/js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+
+  $('.secret-input').bind("enterKey", function (e) {
+    console.log($(".secret-input").val());
+    $(".secret-input").val("");
+  });
+  $('.secret-input').keyup(function (e) {
+    if (e.keyCode == 13) {
+      $(this).trigger("enterKey");
+    }
+  });
+
+});
+
